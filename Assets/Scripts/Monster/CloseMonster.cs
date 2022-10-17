9c3b6c9f8bd5802f3a2a74e9fe3e35d7302b61dc
dy990320 <dy990320@naver.com>
2022-10-17 17:13:09 +0900

    test
    hit test

--- Assets/Scripts/Monster/CloseMonster.cs
+++ Assets/Scripts/Monster/CloseMonster.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class CloseMonster : MonoBehaviour
+=======
+public class CloseMonster : MonsterBase
+>>>>>>> Stashed changes
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +19,15 @@ public class CloseMonster : MonoBehaviour
     {
         
     }
+<<<<<<< Updated upstream
+=======
+    
+    IEnumerator Test()
+    {
+        yield return new WaitForSeconds(5f);
+
+        _attack_speed = 10f;
+
+    }
+>>>>>>> Stashed changes
 }
