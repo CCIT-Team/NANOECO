ff31cc7a168733e84584838cbe25ab5427cf6f30
MoccaHazel <101385960+MoccaHazel@users.noreply.github.com>
2022-10-19 12:48:30 +0900

    무기 테스트 추가

---
+++ Assets/Scripts/Items/Melee.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : WeaponeBase
+{
+    public Transform player;
+
+    void Start()
+    {
+        type = Type.EMELEE;
+    }
+
+    void Update()
+    {
+        /*if(Input.GetKey(KeyCode.Q)&&!isdelay)
+        {
+            isdelay = true;
+            Attack();
+            StartCoroutine("AttackDelay");
+        }*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Monster")
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(knockback*Vector3.Normalize(other.transform.position - player.position),ForceMode.Impulse);
+            other.gameObject.GetComponent<Character>().current_hp -= damage;
+        }
+    }
+
+}
