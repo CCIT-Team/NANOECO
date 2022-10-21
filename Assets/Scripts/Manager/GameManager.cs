eccec77d882ab408ff9824de4cb5112c5ba76310
Kang Seong Jun <dkdlfhqht789@naver.com>
2022-10-21 18:03:09 +0900

    2022-10-21 Start Commit
    -Jun

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,42 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    public class GenericClass<T>
-    {
-        public T m_Value;
-
-        public GenericClass(T value)
-        {
-            m_Value = value;
-        }
-
-        public void Method1()
-        {
-            Debug.Log(m_Value);
-        }
-    }
-
-    public static GameManager gm;
-
-    public Time t;
-
-    private void Awake()
-    {
-        gm = this;
-    }
-
-    void Start()
-    {
-        
-    }
-
-   
-    void Update()
-    {
-        
-    }
-
-
+    public Character[] characters;
 }
