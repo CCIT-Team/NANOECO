ebef7d96202f73b8648e6c0b8781f53f5ce45347
tuost <fldh753@gmail.com>
2022-12-23 13:59:26 +0900

    플레이어 스폰 구현중

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawn : MonoBehaviour
+{ 
+    void Start()
+    {
+        CheckandSpawn();
+    }
+
+    void CheckandSpawn()
+    {
+
+    }
+}
