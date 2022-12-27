81b555a7d454e6463738686097bd5ae2a577fdde
tuost <fldh753@gmail.com>
2022-12-27 18:03:04 +0900

    플레이어 리스폰 구현 완료

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -22,14 +22,14 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
     [Header("Status")]
     public float max_hp;
     public float current_hp;
-    public float damage;
-    public float defense;
     public float jump_force;
     public float dash_force;
     public float move_force;
-    public bool _is_dead;
+    public bool is_dead;
+    public float respawn_time = 5;
     public int skil_num;
     public GameObject[] item;
+    public Transform spawn_point;
     int current_item = 0;
     bool isdash = false;
     public bool isGrounded = true;
@@ -62,12 +62,13 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             Camera.main.GetComponent<PlayerCamera>().player = gameObject.transform;
         }
         instance = this;
+        spawn_point = GameObject.FindGameObjectWithTag("Spawn").transform;
     }
 
     void Start()
     {
         Skil();
-        _is_dead = false;
+        is_dead = false;
         item[0].SetActive(true);
         item[1].SetActive(false);
         item[2].SetActive(false);
@@ -76,10 +77,33 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
 
     void Update()
     {
-        if (pv.IsMine && PhotonNetwork.IsConnected) { Move(); }
+        if (pv.IsMine && PhotonNetwork.IsConnected && !is_dead) { Move(); }
         if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
         //pv.RPC("DestroyRPC", RpcTarget.AllBuffered);
         ItemChange();
+        Dead();
+    }
+
+    void Dead()
+    {
+        if(current_hp <= 0)
+        {
+            is_dead = true;
+            //애니메이션
+            StartCoroutine(ReSpawn());
+        }
+        else
+        {
+            StopCoroutine(ReSpawn());
+        }
+    }
+
+    IEnumerator ReSpawn()
+    {
+        yield return new WaitForSecondsRealtime(respawn_time);
+        is_dead = false;
+        current_hp = max_hp;
+        transform.position = spawn_point.position;
     }
 
     void Move()
--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -9,8 +9,13 @@ public class PlayerCamera : MonoBehaviourPunCallbacks
     public Transform player;
     public float smooth_speed = 0.125f;
     public Vector3 offset;
-
+    public float rotate_speed = 5;
+    
     Vector3 originpos;
+    Vector3 mouseCurserpos;
+    float v;
+    
+    bool inputMode = false;
     private void Start()
     {
         //if(PhotonTestPlayer.instance.pv.IsMine)
@@ -21,7 +26,7 @@ public class PlayerCamera : MonoBehaviourPunCallbacks
     }
     void FixedUpdate()
     {
-        if (player != null)
+        if (player != null && inputMode == false)
         {
             Vector3 desired_position = player.position + offset;
             Vector3 smoothed_position = Vector3.Lerp(transform.position, desired_position, smooth_speed);
@@ -31,36 +36,37 @@ public class PlayerCamera : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        //CameraEvent();
+        CameraEvent();
     }
 
-    //void CameraEvent()
-    //{
-    //    switch(Player.instance.camera_shaking_num)
-    //    {
-    //        case 0:
-    //            StopCoroutine(CameraShaking(0, 0));
-    //            break;
-    //        case 1://무기 사격시 근거리
-    //            StartCoroutine(CameraShaking(0.25f, 0.5f));
-    //            break;
-    //        case 2://무기 사격시 원거리
-    //            StartCoroutine(CameraShaking(0.3f, 1f));
-    //            break;
-    //        case 3://몬스터에게 피격 
-    //            StartCoroutine(CameraShaking(0.2f, 0.175f));
-    //            break;
-    //        case 4://폭탄
-    //            StartCoroutine(CameraShaking(0.5f, 4f));
-    //            break;
-    //        case 5://이벤트 스폰
-    //            StartCoroutine(CameraShaking(0.5f, 0.3f));
-    //            break;
-    //        case 6://이벤트 보스 출현
-    //            StartCoroutine(CameraShaking(0.35f, 1f));
-    //            break;
-    //    }
-   //}
+    void CameraEvent()
+    {
+        switch (Player.instance.camera_shaking_num)
+        {
+            case 0:
+                StopCoroutine(CameraShaking(0, 0));
+                break;
+            case 1://무기 사격시 근거리
+                StartCoroutine(CameraShaking(0.25f, 0.5f));
+                break;
+            case 2://무기 사격시 원거리
+                StartCoroutine(CameraShaking(0.3f, 1f));
+                break;
+            case 3://몬스터에게 피격 
+                StartCoroutine(CameraShaking(0.2f, 0.175f));
+                break;
+            case 4://폭탄
+                StartCoroutine(CameraShaking(0.5f, 4f));
+                break;
+            case 5://이벤트 스폰
+                StartCoroutine(CameraShaking(0.5f, 0.3f));
+                break;
+            case 6://이벤트 보스 출현
+                StartCoroutine(CameraShaking(0.35f, 1f));
+                break;
+        }
+    }
+
 
     IEnumerator CameraShaking(float duration, float manitude)
     {
