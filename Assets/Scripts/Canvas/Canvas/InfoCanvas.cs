c5f4d9d2905a8a62075a690f434adf86e3554ae2
Kang Seong Jun <dkdlfhqht789@naver.com>
2022-12-27 19:16:35 +0900

    닉네임  InputField 값 조정 Resource Sprite 변환 UI작업

---
+++ Assets/Scripts/Canvas/Canvas/InfoCanvas.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//For the Info_Canvas Scripts
+
+public class InfoCanvas : Canvas
+{
+    public void Turn_Off_InfoPanel(GameObject a)
+    {
+        a.SetActive(false);
+    }
+}
