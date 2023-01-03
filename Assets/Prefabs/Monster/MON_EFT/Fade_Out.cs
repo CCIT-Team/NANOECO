e57c3b9c38ed17d3675e372dec6ea24685361466
dy990320 <dy990320@naver.com>
2023-01-03 13:51:56 +0900

    몬스터 분비물 테스트

---
+++ Assets/Prefabs/Monster/MON_EFT/Fade_Out.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade_Out : MonoBehaviour
+{
+    public Color color;
+    public Material mat;
+    public float a = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(test());
+    }
+
+    IEnumerator test()
+    {
+        while (a > 0f)
+        {
+            a -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            color = new(0.29f, 0.74f, 0.24f, a);
+            mat.color = color;
+        }
+    }
+
+}
