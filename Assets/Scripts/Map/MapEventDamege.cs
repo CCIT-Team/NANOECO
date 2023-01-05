6ff9f913590c14a0680d73bbf97e68e27398f547
dy990320 <dy990320@naver.com>
2023-01-05 14:22:34 +0900

    map test

--- Assets/Scripts/Map/MapEventDamege.cs
+++ Assets/Scripts/Map/MapEventDamege.cs
@@ -6,11 +6,7 @@ public class MapEventDamege : MonoBehaviour
 {
     private void OnTriggerStay(Collider other)
     {
-        //±×¸±
-        if (other.gameObject.layer == 6)
-        {
             var player = other.GetComponent<Player>();
             player.current_hp -= 1f;
-        }
     }
 }
