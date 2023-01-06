87c79fa72cc90bd5b5571559407759dfaea1fa8a
MoccaHazel <101385960+MoccaHazel@users.noreply.github.com>
2023-01-06 18:30:20 +0900

    아이템 정리

---
+++ Assets/Scripts/Items/SprayBullet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SprayBullet : Bullet
+{
+    [HideInInspector]
+    public bool canattack = true;
+    new void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        
+    }
+    new void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 8 && canattack)
+        {
+            var monster = other.gameObject.GetComponent<NewMonster>();
+            monster.data.current_hp -= damage;
+            monster.hit_true = true;
+            canattack = false;
+            StartCoroutine("Delayattack");
+        }
+        if (other.gameObject.layer == 11)
+        {
+            other.gameObject.GetComponent<DestroyTarget>()._hp -= damage;
+        }
+    }
+
+    IEnumerator Delayattack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canattack = true;
+    }
+}
