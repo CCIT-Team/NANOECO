a090a65fb36f71272e29312752793a4f29c05055
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2023-01-06 17:41:14 +0900

    no message

--- Assets/Scripts/Mission/MissionSystem.cs
+++ Assets/Scripts/Mission/MissionSystem.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class MissionSystem : MonoBehaviour
 {
-    [Header("화물 운송 미션 클리어")] public bool mission_0_clear = false;
-    [Header("파괴 미션 클리어")] public bool mission_1_clear = false;
-    [Header("화물 운송 클리어")] public bool mission_2_clear = false;
+    [Header("파괴 미션 클리어")] public bool mission_0_clear = false;
+    [Header("호송 미션 클리어")] public bool mission_1_clear = false;
+    [Header("방어 미션 클리어")] public bool mission_2_clear = false;
+    [Header("수리 미션 클리어")] public bool mission_3_clear = false;
+
+    public void Mission_Box_Update()
+    {
+        
+    }
+
+    public void Mission_Clear()
+    {
+
+    }
 }
