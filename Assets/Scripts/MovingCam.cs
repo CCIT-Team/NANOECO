35b2c7ba14f0b0e6bafc35f6364d31ffe1bc31e7
tuost <fldh753@gmail.com>
2023-01-08 02:12:47 +0900

    Update MovingCam.cs

--- Assets/Scripts/MovingCam.cs
+++ Assets/Scripts/MovingCam.cs
@@ -14,15 +14,15 @@ public class MovingCam : MonoBehaviour
 
         Vector3 move = new Vector3(horizontal * moveSpeed * Time.deltaTime, 0, vertical * moveSpeed * Time.deltaTime);
 
-        transform.position += move;
+        transform.position -= move;
 
-        if(Input.GetKeyDown(KeyCode.U))
+        if(Input.GetKey(KeyCode.U))
         {
-            transform.position += Vector3.up* moveSpeed;
+            transform.position -= Vector3.up* moveSpeed *Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKey(KeyCode.I))
         {
-            transform.position += Vector3.down * moveSpeed;
+            transform.position -= Vector3.down * moveSpeed *Time.deltaTime;
         }
     }
 }
